764e5ad415aff1f983b83b0e00048a20c4143298
Lee Young Hyuck <young3@hotmail.com>
2023-02-20 10:50:17 +0900

    update

--- NewsAppMaui/Services/RssService.cs
+++ NewsAppMaui/Services/RssService.cs
@@ -1,41 +1,53 @@
 ﻿using System.Xml;
 using System.ServiceModel.Syndication;
+using System.Text.Json;
 
 namespace NewsAppMaui.Services
 {
     public class RssService: IRssService
     {
-        public RssService() { }
+        HttpClient _client; 
+        IHttpsClientHandlerService _httpsClientHandlerService;
+
+        public RssService(IHttpsClientHandlerService service) 
+        {
+#if DEBUG
+            _httpsClientHandlerService = service;
+            HttpMessageHandler handler = _httpsClientHandlerService.GetPlatformMessageHandler();
+            if (handler != null)
+                _client = new HttpClient(handler);
+            else
+                _client = new HttpClient();
+#else
+            _client = new HttpClient();
+#endif
+        }
 
         public async Task<IEnumerable<SyndicationItem>> GetRssFeedItemsAsync(Uri uri)
         {
-            using (var client = new HttpClient())
+            var rssFeed = await _client.GetStringAsync(uri);
+            var stream = new StringReader(rssFeed);
+
+            SyndicationFeed feed = null;
+            try
             {
-                var rssFeed = await client.GetStringAsync(uri);
-                var stream = new StringReader(rssFeed);                  
-
-                SyndicationFeed feed = null;
-                try
+                using (var reader = XmlReader.Create(stream))
                 {
-                    using (var reader = XmlReader.Create(stream))
-                    {
-                        feed = SyndicationFeed.Load(reader);
-                    }
+                    feed = SyndicationFeed.Load(reader);
                 }
-                catch { } // TODO: Deal with unavailable resource.
-
-                if (feed != null)
-                {
-                    foreach (var element in feed.Items)
-                    {
-                        Console.WriteLine($"Title: {element.Title.Text}");
-                        Console.WriteLine($"Summary: {element.Summary.Text}");
-                    }
-                }
-
-
-                return feed.Items;
             }
+            catch { } // TODO: Deal with unavailable resource.
+
+            if (feed != null)
+            {
+                foreach (var element in feed.Items)
+                {
+                    Console.WriteLine($"Title: {element.Title.Text}");
+                    Console.WriteLine($"Summary: {element.Summary.Text}");
+                }
+            }
+
+            return feed.Items;
         }
     }
 }
