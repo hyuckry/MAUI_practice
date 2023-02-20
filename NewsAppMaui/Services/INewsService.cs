f50cbeb607c73bf11bebc0019d5ca4e7ac715b1b
Lee Young Hyuck <young3@hotmail.com>
2023-02-20 10:49:41 +0900

    NewsService init

--- NewsAppMaui/Services/INewsService.cs
+++ NewsAppMaui/Services/INewsService.cs
@@ -3,19 +3,76 @@ using NewsAppMaui.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+using ThreadNetwork;
 
 namespace NewsAppMaui.Services
 {
     public interface INewsService
     {
-        public ICollection<string> GetTags();
-        public ICollection<Category> GetCategories();
-        public ICollection<Article> GetLatestArticles();
-        public ICollection<Article> GetRecommendedArticles();
-        public ICollection<Article> GetPopularArticles();
-        public ICollection<Article> GetBookmarkedArticles();
-        public string GetArticleBody(string articleId);
+        Task<IEnumerable<string>> GetTags();
+        Task<IEnumerable<Category>> GetCategories();
+        Task<IEnumerable<Article>> GetLatestArticles();
+        Task<IEnumerable<Article>> GetRecommendedArticles();
+        Task<IEnumerable<Article>> GetPopularArticles();
+        Task<IEnumerable<Article>> GetBookmarkedArticles();
+        Task<string> GetArticleBody(string articleId);
+    }
+
+    public class NewsService : INewsService
+    {
+        HttpClient _client;
+        IHttpsClientHandlerService _httpsClientHandlerService;
+
+        public NewsService(IHttpsClientHandlerService service)
+        {
+#if DEBUG
+            _httpsClientHandlerService = service;
+            HttpMessageHandler handler = _httpsClientHandlerService.GetPlatformMessageHandler();
+            if (handler != null)
+                _client = new HttpClient(handler);
+            else
+                _client = new HttpClient();
+#else
+            _client = new HttpClient();
+#endif
+        }
+
+        public Task<string> GetArticleBody(string articleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SyndicationItem>> GetBookmarkedArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Article>> GetLatestArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Article>> GetPopularArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Article>> GetRecommendedArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<string>> GetTags()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- NewsAppMaui/Services/IRssService.cs
+++ NewsAppMaui/Services/IRssService.cs
@@ -9,40 +9,4 @@ namespace NewsAppMaui.Services
     {
         Task<IEnumerable<SyndicationItem>> GetRssFeedItemsAsync(Uri uri);
     }
-
-    public class RssService: IRssService
-    {
-        public RssService() { }
-
-        public async Task<IEnumerable<SyndicationItem>> GetRssFeedItemsAsync(Uri uri)
-        {
-            using (var client = new HttpClient())
-            {
-                var rssFeed = await client.GetStringAsync(uri);
-                var stream = new StringReader(rssFeed);                  
-
-                SyndicationFeed feed = null;
-                try
-                {
-                    using (var reader = XmlReader.Create(stream))
-                    {
-                        feed = SyndicationFeed.Load(reader);
-                    }
-                }
-                catch { } // TODO: Deal with unavailable resource.
-
-                if (feed != null)
-                {
-                    foreach (var element in feed.Items)
-                    {
-                        Console.WriteLine($"Title: {element.Title.Text}");
-                        Console.WriteLine($"Summary: {element.Summary.Text}");
-                    }
-                }
-
-
-                return feed.Items;
-            }
-        }
-    }
 }
