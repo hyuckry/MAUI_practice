270ae8ef4f99cd8d24ed275cd8893dc367003af8
Lee Young Hyuck <young3@hotmail.com>
2023-02-28 16:24:59 +0900

    Init Notes

---
+++ Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+using Notes.Models;
+
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if(BindingContext is About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+        
+    }
+}
