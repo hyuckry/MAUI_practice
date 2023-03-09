aee062d4ea7716b020d649dcdf0692def9a61ac3
Lee Young Hyuck <young3@hotmail.com>
2023-03-09 12:08:49 +0900

    Nuget add

--- DupFinder.Core/FileProcessorLengthFirst.cs
+++ DupFinder.Core/FileProcessorLengthFirst.cs
@@ -5,22 +5,7 @@ using System.Data;
 using System.Text;
 
 namespace DupFinder.Core
-{
-    public static class Constants
-    {
-        public const string DatabaseFilename = "TodoSQLite.db3";
-
-        public const SQLite.SQLiteOpenFlags Flags =
-            // open the database in read/write mode
-            SQLite.SQLiteOpenFlags.ReadWrite |
-            // create the database if it doesn't exist
-            SQLite.SQLiteOpenFlags.Create |
-            // enable multi-threaded database access
-            SQLite.SQLiteOpenFlags.SharedCache;
-
-        public static string DatabasePath =>
-            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
-    }
+{ 
     /// <summary>
     /// This file processor class finds duplicates by
     /// noting all groups of files with the same length
